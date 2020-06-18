2bd75d7a873e9118da17c271a17f944545a7e27b
uditagarwal1305 <55307669+uditagarwal1305@users.noreply.github.com>
2020-06-18 15:25:23 +0530

    index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
-<body>
-<h1>Udit Agarwal</h1>
-<h3>Welcome to my cloud page</h3>
-
-<p>Terraform integrated with AWS</p>
-<br>
+<body style="background-color:grey;">
+  <center><h1>This page is created by Udit Agarwal</h1></center>
+  <center><h3>Welcome to my cloud page</h3></center>
+  <p>This my task to integrate Terraform with AWS</p>
+  <br>
+  <p> This below picture indicate our new startup regarding Covid-19.</p>
 <?php
   $cloudfront_url = `head -n1 myimg.txt`;
   $img_path = "https://".$cloudfront_url."/image.png";
