be59c13f2cce63e6895d2be2082397442b33aa94
uditagarwal1305 <55307669+uditagarwal1305@users.noreply.github.com>
2020-06-22 15:09:16 +0530

    second

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
-<body  bgcolor: #00FFFF>
+<body  bgcolor= "aqua">
   <center><h1>This page is created by Udit Agarwal</h1></center>
   <center><h3>Welcome to my cloud page</h3></center>
   <p>This my task to integrate Terraform with AWS</p>
